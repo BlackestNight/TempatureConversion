8ce2fb17d6a97692bd3a089a6aab18ee67e4d036
Micah bennett <gordenfreeman23@live.com>
2025-07-12 11:27:57 -0500

    made it actually start working

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,24 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <stdio.h> //for the full c expreiance lol
 #include <math.h>
+
 int main()
 {
-    char in[4];
+    char in;
     float num;
-    start:
+    start: //for the goto statement 
     printf("Choose Celsius (C) or Fahrenheit (F) for the input number: ");
     scanf_s("%c", &in, 1);
-    if (strcmp(in, "F") == 0) //checks if the input ai
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+
+    } // an empty function clears the input
+
+    if (in == 'F' || in == 'f') //remember use single quotes when in compaison of char
     {
         printf("Enter the Tempature: ");
         scanf_s("%f", &num);
-        float CalF = num - 32 * .5556;
+        float CalF = (num - 32) * .5556;
         if (round((float)(100 * CalF)) / 100) {   //checks if the conveted number has a zero at the end second decimal point
             printf("%.1f\n", CalF); // if the end is 0 then only print 1 decimal point
         }
@@ -22,17 +26,11 @@ int main()
             printf("%.2f\n", CalF);  //if the end isn't 0 print 2 decimal points
         }
         return 0;
-
-
     }
 
-    else if (strcmp(in, "C") != 0)
+    else if (in == 'C' || in == 'c')
     {
-        printf("Please enter a C or F.");
-       
-    }
-    else {
-         printf("Enter the tempature: "); //asks for the tempature
+        printf("Enter the tempature: "); //asks for the tempature
         scanf_s("%f", &num);    //takes the input
         float CalC = num * 1.8 + 32; //does the conversion of Celsius to Fahernheit
         if (round((float)(100 * CalC)) / 100) {   //checks if the conveted number has a zero at the end second decimal point
@@ -42,5 +40,11 @@ int main()
             printf("%.2f\n", CalC);  //if the end isn't 0 print 2 decimal points
         }
         return 0;
+    }
+
+    else {
+       printf("Please enter a C or F.\n");
+       
+       goto start;
     }
 }
