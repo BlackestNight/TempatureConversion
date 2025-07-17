2d95dd18daa9fcbca7c599b647c37709960ddae4
Micah bennett <gordenfreeman23@live.com>
2025-07-16 20:47:02 -0500

    just slight changes

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,7 +12,7 @@ int main()
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {
 
-    } // an empty function clears the input
+    } // an empty fuction clears the input
 
     if (in == 'F' || in == 'f') //remember use single quotes when in compaison of char
     {
@@ -20,10 +20,10 @@ int main()
         scanf_s("%f", &num);
         float CalF = (num - 32) * .5556;
         if (round((float)(100 * CalF)) / 100) {   //checks if the conveted number has a zero at the end second decimal point
-            printf("%.1f\n", CalF); // if the end is 0 then only print 1 decimal point
+            printf("%.1f C\n", CalF); // if the end is 0 then only print 1 decimal point
         }
         else {
-            printf("%.2f\n", CalF);  //if the end isn't 0 print 2 decimal points
+            printf("%.2f C\n", CalF);  //if the end isn't 0 print 2 decimal points
         }
         return 0;
     }
@@ -34,17 +34,16 @@ int main()
         scanf_s("%f", &num);    //takes the input
         float CalC = num * 1.8 + 32; //does the conversion of Celsius to Fahernheit
         if (round((float)(100 * CalC)) / 100) {   //checks if the conveted number has a zero at the end second decimal point
-            printf("%.1f\n", CalC); // if the end is 0 then only print 1 decimal point
+            printf("%.1f F\n", CalC); // if the end is 0 then only print 1 decimal point
         }
         else {
-            printf("%.2f\n", CalC);  //if the end isn't 0 print 2 decimal points
+            printf("%.2f F\n", CalC);  //if the end isn't 0 print 2 decimal points
         }
         return 0;
     }
 
     else {
        printf("Please enter a C or F.\n");
-       
        goto start;
     }
 }
